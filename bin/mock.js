57f1a655170b5e507e0e0b32358d3ee4fc140d30
David Corwin <david.corwin@gsa.gov>
2020-01-23 09:26:34 -0800

    Resolver

--- bin/mock.js
+++ bin/mock.js
@@ -1,16 +1,13 @@
 const http = require('http');
 
-const {
-  HOST: host = 'localhost',
-  PORT: port = 8001
-} = process.env;
+const { PORT = 8001 } = process.env;
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/html' })
   res.end(`You have reached the ${req.url.slice(1)} bucket.`);
 });
 
-server.listen({host, port}, () => {
+server.listen(PORT, () => {
   console.log('mock server running on ', server.address());
 });
 
