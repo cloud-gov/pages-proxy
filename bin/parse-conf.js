558619df7df4a1999eaef79e2de7b70af3bbd58a
David Corwin <david.corwin@gsa.gov>
2020-01-27 10:12:48 -0800

    Update comments

--- bin/parse-conf.js
+++ bin/parse-conf.js
@@ -25,6 +25,7 @@ const funcs = {
  * Interpolate a templated nginx configuration
  * 
  * @param {string} template - A templated nginx configuration
+ * @param {object} funcs - Configured interpolation functions
  * @returns {string} The interpolated nginx configuration
  */
 function parseConf(template, funcs) {
@@ -45,31 +46,31 @@ function parseConf(template, funcs) {
  * const matches = template.matcnAll(RE);
  * //-> [{ index: 6, name: 'env', arg: 'LANGUAGE', length: 19 }]
  * 
- * const [parts, values] = interpolate(template, matches, funcs);
- * // -> 'Hello javascript world!']
+ * const result = interpolate(template, matches, funcs);
+ * // -> 'Hello javascript world!'
  * 
  * @param {string} template - Any string, but here it will contain the nginx configuration
  * @param {Object[]} matches - An array of results returned from `String.prototype.matchAll()`
- * @param {object} funcs - An object with functions that can be invoked in the template
- * @returns {string} interpolated string
+ * @param {object} funcs - Interpolation functions
+ * @returns {string} The interpolated string
  */
 function interpolate(template, matches, funcs) {
   const results = []
   let start = 0;
 
   for (const match of matches) {
-    // Append the chunk of template between the current and previous matches to `parts`
+    // Append the chunk of template between the current and previous matches.
     results.push(template.slice(start, match.index));
 
     // Determine the next starting index
     start = match.index + match[0].length;
 
     // Find the function named in the match in `funcs` and call it with the
-    // argument from the match. Append the result to `values`.
+    // argument from the match. Append the result.
     results.push(funcs[match.groups.name](match.groups.arg));
   }
   
-  // Append the last template chunk, there will always be one more than the values.
+  // Append the last template chunk.
   results.push(template.slice(start));
   
   return results.join('');
