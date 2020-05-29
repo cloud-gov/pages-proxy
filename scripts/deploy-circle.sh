88589083a35313d6c985e3c14a9720799f0f1acb
David Corwin <david.corwin@gsa.gov>
2020-05-29 08:44:27 -0700

    cf7 (#65)

--- scripts/deploy-circle.sh
+++ scripts/deploy-circle.sh
@@ -23,19 +23,16 @@ else
 fi
 
 # install cf cli
-curl -L -o cf-cli_amd64.deb 'https://cli.run.pivotal.io/stable?release=debian64&source=github'
-sudo dpkg -i cf-cli_amd64.deb
-rm cf-cli_amd64.deb
+curl -L -o cf7.deb 'https://packages.cloudfoundry.org/stable?release=debian64&version=v7&source=github'
+sudo dpkg -i cf7.deb
+rm cf7.deb
 
-# install autopilot
-cf install-plugin autopilot -f -r CF-Community
-
-cf api $CF_API
+cf7 api $CF_API
 
 echo "Logging in to $CF_ORGANIZATION org, $CF_SPACE space."
-cf login -u $CF_USERNAME -p $CF_PASSWORD -o $CF_ORGANIZATION -s $CF_SPACE
+cf7 login -u $CF_USERNAME -p $CF_PASSWORD -o $CF_ORGANIZATION -s $CF_SPACE
 
 echo "Deploying to $CF_SPACE space."
-cf zero-downtime-push $CF_APP --vars-file $CF_VARS_FILE
+cf7 push $CF_APP --strategy rolling --vars-file $CF_VARS_FILE
 
-cf logout
+cf7 logout
