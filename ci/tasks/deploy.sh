c01517de21374dedbab1c79b66705c4f57d28859
Andrew Burnes <andrew.burnes@gsa.gov>
2023-03-13 16:15:40 -0700

    chore: Update app stack to cflinuxfs4

--- ci/tasks/deploy.sh
+++ ci/tasks/deploy.sh
@@ -7,4 +7,7 @@ cf auth
 
 cf t -o $CF_ORG -s $CF_SPACE
 
-cf push $CF_APP_NAME -f $CF_MANIFEST --strategy rolling --vars-file $CF_VARS_FILE
+cf push $CF_APP_NAME -f $CF_MANIFEST \
+  --strategy rolling \
+  --vars-file $CF_VARS_FILE \
+  --stack cflinuxfs4
